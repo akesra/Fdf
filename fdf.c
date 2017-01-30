1396f804cc869fc787d37a0d0eb24e37b7f1022d
Anicet KESRAOUI <akesraou@e3r8p2.42.fr>
2017-01-30 12:11:33 +0100

    move, size and color change

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: akesraou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 14:29:00 by akesraou          #+#    #+#             */
-/*   Updated: 2017/01/27 12:36:42 by akesraou         ###   ########.fr       */
+/*   Updated: 2017/01/30 12:10:23 by akesraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "libft/libft.h"
 #include <stdio.h>
 #include <math.h>
+#include </System/Library/Frameworks/Tk.framework/Versions/Current/Headers/X11/X.h>
 
 typedef		struct	s_env
 {
@@ -32,6 +33,13 @@ typedef		struct	s_env
 	int		yy0;
 	int		yy1;
 	int		spacing;
+	int r;
+	int g;
+	int b;
+	int deep;
+	int iso;
+	int right;
+//	int left;
 }					t_env;
 
 
@@ -125,8 +133,73 @@ int		key_fonction(int keycode, t_env *e)
 {
 	if (keycode == 53)
 		exit (0);
-	if (keycode == 53)
-		mlx_clear_window(e->mlx, e->win);
+	if (keycode == 69)
+	{
+		printf("keycode: %d\n", keycode);
+		if (e->deep < 45)
+			e->deep = e->deep + 1;
+		//	e->r = e->r + 10;
+		//	e->g = e->g - 10;
+	/*	if (e->deep == 0)
+		{
+			e->r = 255;
+			e->g = 0;
+			e->b = 0;
+		}
+		if (e->deep > 0 && e->deep < 10)
+		{
+			e->r = 128;
+			e->r = e->r + 5;
+			e->g = 0;
+			e->b = 0;
+		}
+		if (e->deep > 10 && e->deep < 20)
+		{
+			e->r = 220;
+			e->r = e->r + 5;
+			e->g = 20;
+			e->b = 60;
+		}
+		if (e->deep > 20 && e->deep < 30)
+		{
+			e->r = 255;
+			e->g = 240;
+			e->b = 245;
+		}
+		if (e->deep > 30 && e->deep < 45)
+		{
+			e->r = 255;
+			e->g = 255;
+			e->b = 255;
+		}*/
+		printf("deep++ : %d\n", e->deep);
+	}
+	if (keycode == 78)
+	{
+		printf("keycode: %d\n", keycode);
+		if (e->deep > - 13)
+			e->deep = e->deep - 1;
+			e->b = e->b + 5;
+		printf("deep-- : %d\n", e->deep);
+	}
+	if (keycode == 125)
+	{
+		if (e->iso > 1)
+			e->iso = e->iso - 1;
+		printf("iso: %d\n", e->iso);
+	}
+	if (keycode == 126)
+	{
+		if (e->iso < 20)
+			e->iso = e->iso + 1;
+		printf("iso: %d\n", e->iso);
+	}
+	if (keycode == 124)
+		e->right = e->right + 20;
+	if (keycode == 123)
+		e->right = e->right - 20;
+//	if (keycode == 53)
+//		mlx_clear_window(e->mlx, e->win);
 	return (0);
 }
 
@@ -140,9 +213,9 @@ int		ft_abs(int nb)
 
 void	ft_pixel_put_image(t_env *e, int x, int y)
 {
-	e->data[((x * e->bpp / 8) + (y * e->size_line))] = 255;
-	e->data[((x * e->bpp / 8) + (y * e->size_line)) + 1] = 255;
-	e->data[((x * e->bpp / 8) + (y * e->size_line)) + 2] = 255;
+	e->data[((x * e->bpp / 8) + (y * e->size_line))] = e->b;
+	e->data[((x * e->bpp / 8) + (y * e->size_line)) + 1] = e->g;
+	e->data[((x * e->bpp / 8) + (y * e->size_line)) + 2] = e->r;
 	e->data[((x * e->bpp / 8) + (y * e->size_line)) + 3] = 0;
 }
 
@@ -162,38 +235,25 @@ void	ft_draw_lines(t_env *e)
 	}
 }
 
-void	ft_isomeshit(t_env *e, int *x0, int *y0, int i, int h)
+void	ft_isomeshit(t_env *e, int *x0, int *y0, int i)
 {
 	int x1;
 	int y1;
-	e->xx0 = (((*x0 + e->xmax) - *y0) * 40);
+	e->xx0 = (((*x0 + e->xmax) - *y0) * e->iso + e->right);
+	e->yy0 = (((*x0 + e->xmax) + *y0) * e->iso)  - e->array[*y0][*x0] * e->deep;
 	if (i == 1)
 	{
-		e->yy0 = (((*x0 + e->xmax) + *y0) * 20);// - e->array[*y0][*x0 + 1] * 25;
 		x1 = (*x0 + 1) + e->xmax;
 		y1 = *y0;
-		e->xx1 = ((x1 - y1) * 40);
-		e->yy1 = ((x1 + y1) * 20) - e->array[*y0][*x0 + 1] * 25;
-		if (h == 1)
-		{
-			e->yy0 = (((*x0 + e->xmax) + *y0) * 20) - e->array[*y0][*x0 + 1] * 25;
-			e->xx1 = ((x1 - y1) * 40);
-			e->yy1 = ((x1 + y1) * 20) - e->array[*y0][*x0 + 1] * 25;
-		}
+		e->xx1 = ((x1 - y1) * e->iso + e->right);
+		e->yy1 = ((x1 + y1) * e->iso) - e->array[*y0][*x0 + 1] * e->deep;
 	}
 	if (i == 0)
 	{
-		e->yy0 = (((*x0 + e->xmax) + *y0) * 20);// - e->array[*y0 + 1][*x0] * 25;
 		x1 = *x0 + e->xmax;
 		y1 = *y0 + 1;
-		e->xx1 = ((x1 - y1) * 40);
-		e->yy1 = ((x1 + y1) * 20) - e->array[*y0 + 1][*x0] * 25;
-		if (h == 1)
-		{
-			e->yy0 = (((*x0 + e->xmax) + *y0) * 20) - e->array[*y0 + 1][*x0] * 25;
-			e->xx1 = ((x1 - y1) * 40);
-			e->yy1 = ((x1 + y1) * 20) - e->array[*y0 + 1][*x0] * 25;
-		}
+		e->xx1 = ((x1 - y1) * e->iso + e->right);
+		e->yy1 = ((x1 + y1) * e->iso) - e->array[*y0 + 1][*x0] * e->deep;
 	}
 }
 
@@ -202,9 +262,9 @@ int		ft_draw_map_into_img(t_env *e)
 	int x;
 	int y;
 
-	y = 0;
-	e->img = mlx_new_image(e->mlx, 2000, 1000);
+	e->img = mlx_new_image(e->mlx, 1400, 1000);
 	e->data = mlx_get_data_addr(e->img, &e->bpp, &e->size_line, &e->endian);
+	y = 0;
 	while (y < e->ymax)
 	{
 		x = 0;
@@ -212,16 +272,12 @@ int		ft_draw_map_into_img(t_env *e)
 		{
 			if (x + 1 < e->xmax)
 			{
-				ft_isomeshit(e, &x, &y, 1, 0);
-				ft_draw_lines(e);
-				ft_isomeshit(e, &x, &y, 1, 1);
+				ft_isomeshit(e, &x, &y, 1);
 				ft_draw_lines(e);
 			}
 			if (y + 1 < e->ymax)
 			{
-				ft_isomeshit(e, &x, &y, 0, 0);
-				ft_draw_lines(e);
-				ft_isomeshit(e, &x, &y, 0, 1);
+				ft_isomeshit(e, &x, &y, 0);
 				ft_draw_lines(e);
 			}
 			x++;
@@ -232,10 +288,15 @@ int		ft_draw_map_into_img(t_env *e)
 	return (0);
 }
 
+int		loop_hook(t_env *e)
+{
+	ft_draw_map_into_img(e);
+	return (0);
+}
+
 int		main(int argc, char **argv)
 {
 	t_env	*e;
-	int j;
 
 	if (argc != 2)
 	{
@@ -243,11 +304,17 @@ int		main(int argc, char **argv)
 		return (0);
 	}
 	e = (t_env*)malloc(sizeof(t_env));
+	e->r = 120;
+	e->g = 120;
+	e->b = 120;
+	e->deep = 0;
+	e->iso = 0;
+	e->right = 0;
 	e->mlx = mlx_init();
 	ft_xymax(e, argv[1]);
 	ft_map(e, argv[1]);
-	e->spacing = 5000 / e->xmax;
-	int i = 0;
+//	e->iso = 500 / sqrt(e->xmax * e->xmax + e->ymax * e->ymax);
+/*	int i = 0;
 	while (i < e->ymax)
 	{
 		j = 0;
@@ -258,10 +325,15 @@ int		main(int argc, char **argv)
 		}
 		printf("\n");
 		i++;
-	}
-	e->win = mlx_new_window(e->mlx, 5000, 1000, "FdF");
+	}*/
+	e->win = mlx_new_window(e->mlx, 1400, 1000, "FdF");
 	mlx_key_hook(e->win, key_fonction, e);
-	ft_draw_map_into_img(e);
+//	e->img = mlx_new_image(e->mlx, 2000, 1000);
+//	e->data = mlx_get_data_addr(e->img, &e->bpp, &e->size_line, &e->endian);
+//	ft_draw_map_into_img(e);
+//	mlx_put_image_to_window(e->mlx, e->win, e->img, 0, 0);
+//	mlx_expose_hook(e->win, expose_hook, e);
+	mlx_loop_hook(e->mlx, loop_hook, e);
 	mlx_loop(e->mlx);
 	return (0);
 }
